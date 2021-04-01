c762de2b9f39fa3281b08590b57c6c400b9a04a1
Maira Yicela Rengifo <mairarengifo143@gmail.com>
2021-04-01 14:20:44 -0500

    Se realizan configuraciones para despliegue en Heroku

--- FrontendAplication/server.js
+++ FrontendAplication/server.js
@@ -1,16 +1,19 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 const express = require('express');
-const path = require('path');
- 
 const app = express();
- 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/frontend-aplication'));
- 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/frontend-aplication/index.html'));
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/frontend-aplication'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/frontend-aplication/'}
+);
 });
-// Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
